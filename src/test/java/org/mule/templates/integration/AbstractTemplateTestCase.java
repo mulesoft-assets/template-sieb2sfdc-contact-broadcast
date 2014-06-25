bf108652fdb828439591a0c732f0fcb93b1b0a89
Marek <marecica2@gmail.com>
2014-06-25 13:47:32 +0200

    test fixes

--- src/test/java/org/mule/templates/integration/AbstractTemplateTestCase.java
+++ src/test/java/org/mule/templates/integration/AbstractTemplateTestCase.java
@@ -2,45 +2,32 @@ package org.mule.templates.integration;
 
 import java.io.File;
 import java.io.FileInputStream;
-import java.util.Date;
-import java.util.Map;
 import java.util.Properties;
 
-import org.junit.Rule;
-import org.mule.MessageExchangePattern;
-import org.mule.api.MuleEvent;
 import org.mule.api.config.MuleProperties;
-import org.mule.context.notification.NotificationException;
-import org.mule.processor.chain.SubflowInterceptingChainLifecycleWrapper;
+import org.mule.construct.Flow;
 import org.mule.tck.junit4.FunctionalTestCase;
-import org.mule.tck.junit4.rule.DynamicPort;
-import org.mule.tck.probe.PollingProber;
-import org.mule.tck.probe.Prober;
-import org.mule.templates.test.utils.ListenerProbe;
-import org.mule.templates.test.utils.PipelineSynchronizeListener;
-import org.mule.transport.NullPayload;
 
 /**
- * This is the base test class for Anypoint Templates integration tests.
+ * This is the base test class for Templates integration tests.
+ * 
  * @author damiansima
  */
 public class AbstractTemplateTestCase extends FunctionalTestCase {
+	
 	private static final String MAPPINGS_FOLDER_PATH = "./mappings";
 	private static final String TEST_FLOWS_FOLDER_PATH = "./src/test/resources/flows/";
 	private static final String MULE_DEPLOY_PROPERTIES_PATH = "./src/main/app/mule-deploy.properties";
 
-	protected static final int TIMEOUT_SEC = 120;
-	protected static final String POLL_FLOW_NAME = "mainFlow";
-	protected static final String TEMPLATE_NAME = "contact-broadcast";
-
-	protected final Prober pollProber = new PollingProber(60000, 1000l);
-	protected final PipelineSynchronizeListener pipelineListener = new PipelineSynchronizeListener(POLL_FLOW_NAME);
-
-	protected SubflowInterceptingChainLifecycleWrapper retrieveContactFromBFlow;
-	protected SubflowInterceptingChainLifecycleWrapper retrieveAccountFlowFromB;
-
-	@Rule
-	public DynamicPort port = new DynamicPort("http.port");
+//	@BeforeClass
+//	public static void beforeClass() {
+//		System.setProperty("mule.env", "test");
+//	}
+//
+//	@AfterClass
+//	public static void afterClass() {
+//		System.getProperties().remove("mule.env");
+//	}
 
 	@Override
 	protected String getConfigResources() {
@@ -51,9 +38,10 @@ public class AbstractTemplateTestCase extends FunctionalTestCase {
 			resources = props.getProperty("config.resources");
 		} catch (Exception e) {
 			throw new IllegalStateException(
-					"Could not find mule-deploy.properties file on classpath. Please add any of those files or override the getConfigResources() method to provide the resources by your own.");
+				"Could not find mule-deploy.properties file on classpath. " +
+				"Please add any of those files or override the getConfigResources() " +
+				"method to provide the resources by your own.");
 		}
-
 		return resources + getTestFlows();
 	}
 
@@ -64,17 +52,15 @@ public class AbstractTemplateTestCase extends FunctionalTestCase {
 		File[] listOfFiles = testFlowsFolder.listFiles();
 		if (listOfFiles != null) {
 			for (File f : listOfFiles) {
-				if (f.isFile() && f.getName()
-									.endsWith("xml")) {
+				if (f.isFile() && f.getName().endsWith("xml")) {
 					resources.append(",")
 								.append(TEST_FLOWS_FOLDER_PATH)
 								.append(f.getName());
 				}
 			}
 			return resources.toString();
-		} else {
-			return "";
-		}
+		} 
+		return "";
 	}
 
 	@Override
@@ -89,55 +75,8 @@ public class AbstractTemplateTestCase extends FunctionalTestCase {
 		return properties;
 	}
 
-	protected void registerListeners() throws NotificationException {
-		muleContext.registerListener(pipelineListener);
-	}
-
-	protected void waitForPollToRun() {
-		System.out.println("Waiting for poll to run ones...");
-		pollProber.check(new ListenerProbe(pipelineListener));
-		System.out.println("Poll flow done");
-	}
-
-	@SuppressWarnings("unchecked")
-	protected Map<String, Object> invokeRetrieveFlow(SubflowInterceptingChainLifecycleWrapper flow, Map<String, Object> payload) throws Exception {
-		MuleEvent event = flow.process(getTestEvent(payload, MessageExchangePattern.REQUEST_RESPONSE));
-		Object resultPayload = event.getMessage()
-									.getPayload();
-
-		if (resultPayload instanceof NullPayload) {
-			return null;
-		} else {
-			return (Map<String, Object>) resultPayload;
-		}
-	}
-
-	protected String buildUniqueName(String templateName, String name) {
-		String timeStamp = new Long(new Date().getTime()).toString();
-
-		StringBuilder builder = new StringBuilder();
-		builder.append(name);
-		builder.append(templateName);
-		builder.append(timeStamp);
-
-		return builder.toString();
-	}
-
-	protected String buildUniqueEmail(String user) {
-		String server = "fakemail";
-		String timeStamp = new Long(new Date().getTime()).toString();
-
-		StringBuilder builder = new StringBuilder();
-		builder.append(user);
-		builder.append(".");
-		builder.append(timeStamp);
-		builder.append("@");
-		builder.append(server);
-		builder.append(TEMPLATE_NAME);
-		builder.append(".com");
-
-		return builder.toString();
-
+	protected Flow getFlow(String flowName) {
+		return (Flow) muleContext.getRegistry().lookupObject(flowName);
 	}
 
 }
